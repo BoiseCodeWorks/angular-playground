e39a2f4777b4a268bf5ffdd432765e5adfe20d14
Jake Overall <joverall22@gmail.com>
2015-09-24 16:55:48 -0600

    adds repeat challenge

--- app/components/repeat/RepeatController.js
+++ app/components/repeat/RepeatController.js
@@ -4,5 +4,57 @@ app.controller('RepeatController', RepeatController);
 
 
 function RepeatController(){
-	this.repeatStatus = 'Working';
+  this.repeatStatus = 'Working';
+  //The cart is an array of items that we are adding from our view
+  this.cart = [];
+  //The store is what we are using to populate our page with
+  this.store = {
+    items: StoreData
+  };
+  //*** No Need to edit anything above this line ****
+  
+  this.name = "My BɼokƏn Cart!";
+  
+  this.getCartCount = function(){
+    //return the length of our cart
+  };
+  
+  /*
+  * Write a calculateCartTotal function
+  * make it assesible to our view
+  * this function should return the total cost
+  * of each item that is in our cart
+  */ 
+  
+  
+  this.removeItemFromCart = function(item){
+    //Item gets passed in from our view when the user clicks the x button
+    /*
+    * This function should be able to remove the passed in item
+    * from our cart. You will have to first identify where the passed
+    * in item is in the array. Then you will need to use the correct
+    * Array.method to remove 1 item hint method(i, 1);
+    */
+  };
+  
+  this.addItemToCart = function(item){
+      //item gets passed in to this function from the view
+      
+      /*
+      Our cart demands that items being added to it must have the following properties
+      var newItem = {
+        name:'',
+        color:'',
+        size: '',
+        quantity: 1,
+        price:'',
+      }
+      console.log the item being passed in from the view 
+      to figure out which properties from item need to be 
+      remaped to the newItem object. 
+      After building the newItem add it to the cart 
+      */
+      
+  }
+  
 }
---
+++ app/components/repeat/StoreData.js
@@ -0,0 +1,23 @@
+var StoreData = {
+    1001:{
+        name:"T-Shirt",
+        colors:[{name:"White"},{name:"Blue"},{name:"Green"}, {name:"Black"}],
+        sizes:["S","M","L","XL"],
+        price: 6.79,
+        img:"https://www.qwertee.com/images/mens-black.png"
+      },
+    1002:{
+        name:"Bowling Ball",
+        colors:[{name:"Red"},{name:"Blue"},{name:"Purple"}, {name:"Black"}],
+        sizes:["8 lb","10 lb","12 lb","14 lb"],
+        price: 27.79,
+        img:"http://cliparts.co/cliparts/kiK/B8E/kiKB8E8oT.png"
+      },
+    1003:{
+        name:"Pirate Cap With Patch",
+        colors:[{name:"Red"},{name:"Blue"},{name:"Pink"},{name:"Black"}],
+        sizes:["S"],
+        price: 1299.99,
+        img:"http://www.clipartbest.com/cliparts/aTe/RjM/aTeRjM7T4.png"
+      }
+  }
