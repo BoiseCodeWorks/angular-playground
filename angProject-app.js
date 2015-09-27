c4af2fe556229621ddf80fb04439e6ae0b1d7772
Mike Vickery <mvickery@trugrocer.com>
2015-09-26 20:35:07 -0600

    all complete for now - ready to tackle services

---
+++ angProject-app.js
@@ -0,0 +1,47 @@
+﻿var app = angular.module('racingFrogs', []);
+app.controller('MainController', MainController);
+//No need to change anything above this line.
+
+function MainController() {
+    var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
+    vm.working = "Yes";
+    vm.joe = new Guy("Joe", 100);
+    vm.bob = new Guy("Bob", 150);
+    vm.bank = 200;
+
+    function Guy(name, startingCash) {
+        this.name = name;
+        this.cash = startingCash;
+        this.giveCash = function (amount) {
+            if (amount <= this.cash && amount > 0) {
+                this.cash = this.cash - amount;
+                return amount;
+            } else {
+                console.log("I don't have enough cash to give you " + amount + ". " + this.name + " says...");
+                return 0;
+            }
+        };
+        this.receiveCash = function (amount) {
+            if (amount > 0) {
+                this.cash = this.cash + amount;
+                return amount;
+            } else {
+                console.log(amount + " isn't an amount I'll take " + this.name + " says...");
+                return 0;
+            }
+        }
+    }
+
+    vm.giveMoneyToJoe = function () {
+        if (vm.bank >= 10) {
+            vm.bank -= vm.joe.receiveCash(10);
+        } else {
+            alert("The bank is out of money.");
+        }
+    }
+
+    vm.receiveMoneyFromBob = function () {
+        vm.bank += vm.bob.giveCash(5);
+    }
+
+}
--- app/components/repeat/RepeatController.js
+++ app/components/repeat/RepeatController.js
@@ -1,60 +1,87 @@
-var app = angular.module('angularPlayground');
+﻿var app = angular.module('angularPlayground');
 
 app.controller('RepeatController', RepeatController);
 
 
-function RepeatController(){
-  this.repeatStatus = 'Working';
-  //The cart is an array of items that we are adding from our view
-  this.cart = [];
-  //The store is what we are using to populate our page with
-  this.store = {
-    items: StoreData
-  };
-  //*** No Need to edit anything above this line ****
-  
-  this.name = "My BɼokƏn Cart!";
-  
-  this.getCartCount = function(){
-    //return the length of our cart
-  };
-  
-  /*
-  * Write a calculateCartTotal function
-  * make it assesible to our view
-  * this function should return the total cost
-  * of each item that is in our cart
-  */ 
-  
-  
-  this.removeItemFromCart = function(item){
-    //Item gets passed in from our view when the user clicks the x button
+function RepeatController() {
+    this.repeatStatus = 'Working';
+    //The cart is an array of items that we are adding from our view
+    this.cart = [];
+    //The store is what we are using to populate our page with
+    this.store = {
+        items: StoreData
+    };
+    //*** No Need to edit anything above this line ****
+
+    this.name = "My BɼokƏn Cart!";
+
+    this.getCount = function () {
+        //return the length of our cart
+        return this.cart.length;
+    };
+
     /*
-    * This function should be able to remove the passed in item
-    * from our cart. You will have to first identify where the passed
-    * in item is in the array. Then you will need to use the correct
-    * Array.method to remove 1 item hint method(i, 1);
+    * Write a calculateCartTotal function
+    * make it assesible to our view
+    * this function should return the total cost
+    * of each item that is in our cart
     */
-  };
-  
-  this.addItemToCart = function(item){
-      //item gets passed in to this function from the view
-      
-      /*
-      Our cart demands that items being added to it must have the following properties
-      var newItem = {
-        name:'',
-        color:'',
-        size: '',
-        quantity: 1,
-        price:'',
-      }
-      console.log the item being passed in from the view 
-      to figure out which properties from item need to be 
-      remaped to the newItem object. 
-      After building the newItem add it to the cart 
-      */
-      
-  }
-  
+
+    this.calculateCartTotal = function () {
+        var total = 0;
+        for (var i = 0; i < this.cart.length; i++) {
+            total += this.cart[i].price * this.cart[i].quantity;
+            //console.log(i + " Item:"+this.cart[i].name+" Price:"+this.cart[i].price+" Quantity:"+this.cart[i].quantity + " RunningTotal:"+ total);
+        }
+        return total;
+    };
+
+    this.removeItemFromCart = function (item) {
+        //Item gets passed in from our view when the user clicks the x button
+        for (var i = 0; i < this.cart.length; i++) {
+            if (item.name === this.cart[i].name &&
+                item.color === this.cart[i].color &&
+                item.size === this.cart[i].size &&
+                item.quantity === this.cart[i].quantity &&
+                item.price === this.cart[i].price) {
+                //console.log("Delete " + i + " which is " + item.name + ":" + item.size + ":" + item.color);
+                this.cart.splice(i, 1);
+                break;
+            }
+        }
+        /*
+        * This function should be able to remove the passed in item
+        * from our cart. You will have to first identify where the passed
+        * in item is in the array. Then you will need to use the correct
+        * Array.method to remove 1 item hint method(i, 1);
+        */
+    };
+
+    this.addItemToCart = function (item) {
+        //item gets passed in to this function from the view
+        //console.log(item);
+        var newItem = {
+            name: item.name,
+            color: item.selectedColor,
+            size: item.selectedSize,
+            quantity: 1,
+            price: item.price,
+        }
+        //console.log(newItem);
+        this.cart.push(newItem);
+        /*
+        Our cart demands that items being added to it must have the following properties
+        var newItem = {
+          name:'',
+          color:'',
+          size: '',
+          quantity: 1,
+          price:'',
+        }
+        console.log the item being passed in from the view 
+        to figure out which properties from item need to be 
+        remaped to the newItem object. 
+        After building the newItem add it to the cart 
+        */
+    }
 }
