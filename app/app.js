078ec83369f90dbe91fd2bd9052379ed25db15e0
Jake Overall <joverall22@gmail.com>
2016-07-18 00:21:11 -0600

    implements ui-router

--- app/app.js
+++ app/app.js
@@ -1,7 +1,17 @@
 (function () {
     
-angular.module('angularPlayground', ['Prism'])
+angular.module('angularPlayground', ['Prism', 'ui.router'])
+.constant('navStates', ['home', 'start', 'expressions', 'bindings', 'repeat', 'cart'])
+.config(function($stateProvider, navStates){
 
+	navStates.forEach(function(state){
+		$stateProvider.state({
+			name: state,
+			url: `/${state}`,
+			template: `<${state}-component></${state}-component>`
+		})
+	})
+})
 .controller('MainController', MainController);
 
 function MainController(){
--- app/utilities/view-manager.js
+++ app/utilities/view-manager.js
@@ -1,16 +1,22 @@
 ;(function () {
   angular.module('angularPlayground')
-    .controller('ViewManager', ViewManager)
+    .component('navbarComponent', {
+      template: `
+			<div class="panel panel-heading">
+				<ul class="nav nav-pills">
+					<li ng-repeat="state in vm.navStates" ng-class="{'active': vm.view === state}"><a ui-sref="{{state}}">{{state}}</a></li>
+				</ul>
+			</div>
+			`,
+      controller: ViewManager,
+      controllerAs: 'vm'
+    })
 
-  function ViewManager () {
-    this.changeView = function (view) {
-      this.view = view
-    }
-
-    if (location.hash) {
-      this.view = location.hash.substr(2)
-    } else {
-      this.view = 'home'
-    }
+  function ViewManager ($scope, $state, navStates) {
+		var vm = this;
+		this.navStates = navStates;
+		$scope.$on('$stateChangeSuccess', function(){
+    	vm.view = $state.current.name;
+		})
   }
 }())
